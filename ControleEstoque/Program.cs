216d7d2a763f2040ee35e18242b407b6b24832c6
Samuel Zaire <96780102+szaire@users.noreply.github.com>
2023-05-19 18:24:40 -0300

    add backup files

---
+++ ControleEstoque/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		// ETAPAS DE DESENVOLVIMENTO:
+		// - TODO: Modelar o diagrama de Classes
+		// - TODO: Modelar o fluxograma
+		// - TODO: Modelar a serialização (JSON)
+		// - TODO: Criar os pseudocógicos
+		// - TODO: Implementar o código
+
+		
+	}
+}
